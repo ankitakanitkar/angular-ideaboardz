0496a13c4ca1064b89b81dbee3b56ce51a60f532
Ankita kanitkar <ankita@ubuntu.(none)>
2013-02-06 05:45:56 -0800

    added edit functionality for cards

--- js/idea.js
+++ js/idea.js
@@ -1,21 +1,51 @@
 var myapp = angular.module('myapp', ['ui']);
 
-myapp.controller('IdeaCtrl', function ($scope){
+myapp.controller('IdeaCtrl', function ($scope,lightBox){
 	$scope.userName = 'Subuanks';
-	$scope.ideas = [{id: 0 ,text: "Idea 1" , votes: 1},{id: 1 ,text : "Idea2" , votes: 0},{id: 2 ,text: "Idea3" , votes: 0}];
+	$scope.lightbox = lightBox;
+	$scope.ideas = [{id: 0 ,text: "Idea 1" , votes: 1, status: true},
+					{id: 1 ,text: "Idea 2" , votes: 0, status: true},
+					{id: 2 ,text: "Idea 3" , votes: 0, status: true}];
+
 	
 	$scope.removeText = function(){
 		$scope.FormIdeaText = '';
 	}
 	
 	$scope.addIdea = function(){
-		$scope.ideas.push({text: $scope.FormIdeaText , votes:0});
+		newId = $scope.ideas.length;
+		$scope.ideas.push({id: newId , text: $scope.FormIdeaText , votes:0});
 		$scope.FormIdeaText = '';
 	}
 	
 	$scope.voteUp = function(element){
 		$scope.ideas[element.id].votes = element.votes + 1;
 	}
+
+	$scope.editCard = function(element){
+		$scope.edittedIdeaText = "";
+		lightBox.ideaForm.on = true;
+		lightBox.ideaForm.id = element.id;
+			
+	}
+	$scope.updateIdeaText = function(){
+		newText = $scope.edittedIdeaText;
+		ideaId = lightBox.ideaForm.id;
+		$scope.ideas[ideaId].text = newText;
+		lightBox.ideaForm.on = false;
+	}
+});
+
+myapp.factory('lightBox', function () {
+    return {
+        ideaForm: {
+            on: false,
+            id: 0
+        },
+        close: function (boxName) {
+            this[boxName].on = false;
+        }
+    };
 });
 
 angular.bootstrap(document, ['myapp']);
